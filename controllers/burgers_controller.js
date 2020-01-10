cfe70d6c906d910f97d87a9b932dcc7de247db0e
Anna Lebiedziewicz <nalin.kane@gmail.com>
2020-01-10 20:48:03 +0000

    simple handlebard templates, client js and styles

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get("/", function(req, res) {
   burger.all(function(data) {
-    var hbsObject = {
+    const hbsObject = {
       burger: data
     };
     console.log(hbsObject);
--- server.js
+++ server.js
@@ -2,23 +2,21 @@ require("dotenv").config();
 
 const express = require("express");
 const handlebars = require("express-handlebars");
+const routes = require("./controllers/burgers_controller.js");
 
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
-var app = express();
+const app = express();
 
 app.use(express.static("public"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burgers_controller.js");
-
 app.use(routes);
 
 app.listen(PORT, function() {
-  console.log("Server listening on: http://localhost:" + PORT);
+  console.log(`Server listening on: http://localhost:${PORT}`);
 });
