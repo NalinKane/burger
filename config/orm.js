1333b9d74debb23f89a15edb5439712a1382bada
Anna Lebiedziewicz <nalin.kane@gmail.com>
2020-01-10 23:48:22 +0000

    remove comments

--- config/orm.js
+++ config/orm.js
@@ -13,22 +13,16 @@ function printQuestionMarks(num) {
 function objToSql(ob) {
   var arr = [];
 
-  // loop through the keys and push the key/value as a string int arr
   for (var key in ob) {
     var value = ob[key];
-    // check to skip hidden properties
     if (Object.hasOwnProperty.call(ob, key)) {
-      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
       if (typeof value === "string" && value.indexOf(" ") >= 0) {
         value = "'" + value + "'";
       }
-      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-      // e.g. {sleepy: true} => ["sleepy=true"]
       arr.push(key + "=" + value);
     }
   }
 
-  // translate array of strings to a single comma-separated string
   return arr.toString();
 }
 
