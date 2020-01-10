aef553fab02c3bf302e19d7ada437f1317c6fbe9
Anna Lebiedziewicz <nalin.kane@gmail.com>
2020-01-10 21:27:42 +0000

    Add simple client side js code to submit new burger

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,1 +1,28 @@
-console.log("client js");
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  const form = document.querySelector("#form");
+  form.addEventListener("submit", addNewBurger);
+}
+
+function addNewBurger(e) {
+  const burgerInput = document.querySelector("#newBurger");
+  e.preventDefault();
+  console.log(burgerInput.value);
+
+  const data = {
+    burger_name: burgerInput.value
+  };
+
+  fetch("/api/burgers", {
+    method: "POST",
+    body: JSON.stringify(data)
+  })
+    .then(() => {
+      console.log("Added new burger");
+      location.reload();
+    })
+    .catch(error => {
+      console.error("Error:", error);
+    });
+}
