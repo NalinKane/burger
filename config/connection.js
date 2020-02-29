16cbe9fb04081cabe9280b5e516122dbabacceb3
Anna Lebiedziewicz <nalin.kane@gmail.com>
2020-02-29 10:46:42 +0000

    re-did the connection

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,39 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+const db_config = {
   host: process.env.DB_HOST,
   port: 3306,
   user: process.env.DB_USERNAME,
   password: process.env.DB_LOCAL_PASSWORD,
   database: process.env.DB_DATABASE_NAME
-});
-// mysql://b967ab1084995b:a48310f7@eu-cdbr-west-02.cleardb.net/heroku_334660c09ff8a89?reconnect=true
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+};
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+  // the old one cannot be reused.
+
+  connection.connect(function(err) {
+    // The server is either down
+    if (err) {
+      // or restarting (takes a while sometimes).
+      console.log("error when connecting to db:", err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    } // to avoid a hot loop, and to allow our node script to
+  }); // process asynchronous requests in the meantime.
+  // If you're also serving http, display a 503 error.
+  connection.on("error", function(err) {
+    console.log("db error", err);
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      // Connection to the MySQL server is usually
+      handleDisconnect(); // lost due to either server restart, or a
+    } else {
+      // connnection idle timeout (the wait_timeout
+      throw err; // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
 
 module.exports = connection;
